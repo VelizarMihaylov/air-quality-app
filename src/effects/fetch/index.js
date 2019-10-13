c59a6848b0684cedf9f6ad7e6e6f39f6c9843dc3
VelizarMihaylov <velizar.mihaylov@gmail.com>
2019-10-13 14:20:16 +0100

    Aded description for withFetch HOC

--- src/effects/fetch/index.js
+++ src/effects/fetch/index.js
@@ -1,6 +1,32 @@
 import React from 'react'
 import useFetch from './useFetch'
-// @TODO Add some description about this HOC
+
+/*
+    ****** withFetch Higher Order Component ******
+
+  This is an attempt to centralize all fetch side effects.
+  If a component depends on some external data from
+  third party API you should be able to wrap it with withFetch,
+  and not worry about handling the logic around that in your component
+
+  An example use:
+  javascript```
+  // The wrapped component will automatically receive
+  // loading, error and data props
+  // those will represent all possible stages of the fetch
+  const MyAwesomeComponent = ({ loading, error, data, ...props}) => ...
+
+  // Wrap the component with withFetch before exporting it
+  export default withFetch(MyAwesomeComponent)
+
+  //Then when you use the component all you need to do is passing url prop
+  <MyAwesomeComponent url='https://someaowsomeapi.com' />
+  // Or render conditionally based on query param
+  {postId && <MyAwesomeComponent url=`https://someaowsomeapi.com/post?id=${postId}` />}
+  ```
+*/
+
+
 export const withFetch = WrappedComponent => props => {
   const { url } = props
   if (!url || typeof url !== 'string') {
