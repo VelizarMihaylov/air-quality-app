4719ddfde3d3af2db013122ad4225c7df7c9be42
VelizarMihaylov <velizar.mihaylov@gmail.com>
2019-10-17 07:54:08 +0100

    Refactoring the searchbox to a custom react component

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import withFetch from 'effects/fetch'
 import { connect } from 'react-redux'
 
@@ -11,28 +11,51 @@ export const SearchBox = ({
   data,
   dispatch
 }) => {
+  const [dataList, setDataList] = useState([])
+  const [selection, setSelection] = useState('')
+  const [showList, setShowList] = useState(false)
   if (error) return (<h1>Oops something went wrong!</h1>)
   const { results: cities } = data || { results: [] }
   return (
-    <>
-      <form className='SearchBox'>
+    <div className='SearchBox'>
+      <form className='SearchBox--form'>
         {loading ? <div className='SearchBox--icon__spinner' /> : <MagnifyingGlassIcon className='SearchBox--icon__magnifyingGlass' />}
         <input
           disabled={loading}
           className='SearchBox--input'
           autoComplete='off'
-          list='cities'
           name='cities'
           placeholder={loading ? undefined : 'Enter city name...'}
-          onSelect={event => (
-            cities.find(city => city.name === event.target.value) && dispatch({ type: 'ADD_CITY', payload: event.target.value })
-          )}
+          onChange={event => {
+            setDataList(cities.filter(({ name }) => name.match(RegExp(event.target.value, 'i'))))
+            setSelection(event.target.value)
+          }}
+          onBlur={event => setShowList(false)}
+          onFocus={event => {
+            if (!dataList.length) setDataList(cities)
+            setShowList(true)
+          }}
+          value={selection}
         />
-        <datalist id='cities'>
-          {cities.map(({ name, i }) => (<option key={`${i}-${name}`} value={name} />))}
-        </datalist>
       </form>
-    </>
+      <ul
+        className='SearchBox--datalist'
+        style={{
+          display: showList ? 'block' : 'none'
+        }}
+      >
+        {dataList.map(({ name }) =>
+          <li
+            key={name}
+            onMouseDown={event => {
+              setSelection(name)
+              dispatch({ type: 'ADD_CITY', payload: name })
+            }}
+          >{name}
+          </li>
+        )}
+      </ul>
+    </div>
   )
 }
 
